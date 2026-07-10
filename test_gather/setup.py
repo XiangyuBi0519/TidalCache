2f87cfbdbee1ee925c393eac3e8f5767bb9a3b01
Xiangyu Bi <136948167+XiangyuBi0519@users.noreply.github.com>
2026-07-10 15:40:54 +0800

    Update setup.py

--- test_gather/setup.py
+++ test_gather/setup.py
@@ -1,10 +1,3 @@
-"""
-Build script for GatherSelectionKvCache test wrapper.
-
-Usage on NPU machine:
-    export LD_LIBRARY_PATH=/usr/local/Ascend/cann-8.5.1/opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}
-    python setup.py build_ext --inplace
-"""
 import os
 import torch
 from setuptools import setup
@@ -13,41 +6,25 @@ from torch.utils.cpp_extension import CppExtension, BuildExtension
 CANN_HOME = os.environ.get("ASCEND_HOME_PATH", "/usr/local/Ascend/cann-8.5.1")
 CUSTOM_OPP = os.path.join(CANN_HOME, "opp", "vendors", "customize")
 
-# torch_npu include/lib paths
 try:
     import torch_npu
     torch_npu_dir = os.path.dirname(torch_npu.__file__)
-    torch_npu_include = os.path.join(torch_npu_dir, "include")
-    torch_npu_lib = os.path.join(torch_npu_dir, "lib")
 except ImportError:
     raise RuntimeError("torch_npu is not installed")
 
-# CANN include/lib paths
-cann_include = os.path.join(CANN_HOME, "include")
-cann_lib = os.path.join(CANN_HOME, "lib64")
-
-# Custom op include/lib paths
-custom_op_include = os.path.join(CUSTOM_OPP, "op_api", "include")
-custom_op_lib = os.path.join(CUSTOM_OPP, "op_api", "lib")
-
-# aclnn_torch_adapter include path (for EXEC_NPU_CMD macro)
-# This is typically in torch_npu or in the CANN opp directory
-aclnn_adapter_include = os.path.join(CANN_HOME, "opp", "built-in", "op_api", "include")
-
 include_dirs = [
-    torch_npu_include,
-    cann_include,
-    custom_op_include,
-    aclnn_adapter_include,
+    os.path.join(torch_npu_dir, "include"),
+    os.path.join(torch_npu_dir, "include", "third_party", "acl", "inc"),
+    os.path.join(CANN_HOME, "include"),
+    os.path.join(CUSTOM_OPP, "op_api", "include"),
 ]
 
 library_dirs = [
-    torch_npu_lib,
-    cann_lib,
-    custom_op_lib,
+    os.path.join(torch_npu_dir, "lib"),
+    os.path.join(CANN_HOME, "lib64"),
+    os.path.join(CUSTOM_OPP, "op_api", "lib"),
 ]
 
-# Filter to only include existing directories
 include_dirs = [d for d in include_dirs if os.path.isdir(d)]
 library_dirs = [d for d in library_dirs if os.path.isdir(d)]
 
@@ -62,8 +39,8 @@ setup(
             sources=["gather_wrapper.cpp"],
             include_dirs=include_dirs,
             library_dirs=library_dirs,
-            libraries=["cust_opapi", "ascendcl", "torch_npu"],
-            extra_compile_args=["-std=c++17", "-D__EXPORT_API__"],
+            libraries=["cust_opapi", "nnopbase", "ascendcl"],
+            extra_compile_args=["-std=c++17"],
         )
     ],
     cmdclass={"build_ext": BuildExtension},
